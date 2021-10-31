b32bd669d59aa3e8df1b28ce7ccd4b611e4d18b5
Niall <nialldbarber@gmail.com>
2021-10-31 20:09:21 +0000

    Updated script

--- server.ts
+++ server.ts
@@ -7,21 +7,6 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-// let interval;
-// client.on('ready', async (msg: any) => {
-//   const lotto = await doesJackpotExceedLimits(
-//     'https://www.national-lottery.co.uk/games/euromillions',
-//   );
-
-//   interval = setInterval(function () {
-//     if (lotto) {
-//       msg.channel.send('Euromillions is over £100 mill!');
-//     } else {
-//       msg.channel.send("Euromillions is shit, don't bother");
-//     }
-//   }, 10000); //every hour
-// });
-
 let interval: any;
 
 client.on('message', async (msg: any) => {
